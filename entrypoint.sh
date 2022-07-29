d35bc2cbd69ba53ba473544013d93ed8aab7bcc0
airbone95 <airbone95@users.noreply.github.com>
2022-07-29 10:28:51 +0200

    fixing the link

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,8 @@ e() {
 }
 
 ## retrieve token from dockerhub to do operations
-echo "retrieving token from https://ghcr.io/token?service=ghcr.io&scope=repository:$INPUT_DOCKERHUB_REPO:pull,push"
-TOKEN=$(curl -sL -u "$INPUT_DOCKERHUB_USERNAME:$INPUT_DOCKERHUB_PASSWORD" "https://auth.ghcr.io/token?service=registry.ghcr.io&scope=repository:$INPUT_DOCKERHUB_REPO:pull,push" | tee $TMPFILE | jq --raw-output .token)
+echo "retrieving token from https://ghcr.io/token?scope=repository:$INPUT_GHCR_REPO:pull,push"
+TOKEN=$(curl -sL -u "$INPUT_GHCR_USERNAME:$INPUT_GHCR_PASSWORD" "https://ghcr.io/token?service=ghcr.io&scope=repository:$INPUT_GHCR_REPO:pull,push" | tee $TMPFILE | jq --raw-output .token)
 if [ -z "$TOKEN" ]; then
   echo "error recieving token:"
   cat $TMPFILE
@@ -20,8 +20,8 @@ else
 fi
 
 ## retrieve manifest of docker image
-echo "retrieving manifest from https://ghcr.io/v2/$INPUT_DOCKERHUB_REPO/manifests/$INPUT_OLD_TAG"
-RC=$(curl -sL -H "Authorization: Bearer $TOKEN" -H "Accept: application/vnd.docker.distribution.manifest.v2+json"  "https://ghcr.io/v2/$INPUT_DOCKERHUB_REPO/manifests/$INPUT_OLD_TAG" -o $TMPFILE -w "%{http_code}")
+echo "retrieving manifest from https://ghcr.io/v2/$INPUT_GHCR_REPO/manifests/$INPUT_OLD_TAG"
+RC=$(curl -sL -H "Authorization: Bearer $TOKEN" -H "Accept: application/vnd.docker.distribution.manifest.v2+json"  "https://ghcr.io/v2/$INPUT_GHCR_REPO/manifests/$INPUT_OLD_TAG" -o $TMPFILE -w "%{http_code}")
 if [ "$RC" -eq 200 ]; then
   echo "manifest for $INPUT_OLD_TAG retrieved successfully"
 else
@@ -31,8 +31,8 @@ else
 fi
 
 ## push manifest with new tag to dockerhub
-echo "pushing manifest to https://ghcr.io/v2/$INPUT_DOCKERHUB_REPO/manifests/$INPUT_NEW_TAG"
-RC=$(curl -sL -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/vnd.docker.distribution.manifest.v2+json"  "https://ghcr.io/v2/$INPUT_DOCKERHUB_REPO/manifests/$INPUT_NEW_TAG" -X PUT -d "@$TMPFILE" -o $TMPFILE2 -w "%{http_code}")
+echo "pushing manifest to https://ghcr.io/v2/$INPUT_GHCR_REPO/manifests/$INPUT_NEW_TAG"
+RC=$(curl -sL -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/vnd.docker.distribution.manifest.v2+json"  "https://ghcr.io/v2/$INPUT_GHCR_REPO/manifests/$INPUT_NEW_TAG" -X PUT -d "@$TMPFILE" -o $TMPFILE2 -w "%{http_code}")
 if [ "$RC" -eq 201 ]; then
   echo "new tag $INPUT_NEW_TAG created successfully"
 else
